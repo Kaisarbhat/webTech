65e5a6694cdc910118857abb8634d4607ba74b4d
Kaisar bhat <kaisarahmadbhat1108@gmail.com>
2023-04-04 17:49:23 +0530

    validation

--- bst/Screens/script.js
+++ bst/Screens/script.js
@@ -17,3 +17,30 @@ btnPopup.addEventListener('click',()=>{
 iconClose.addEventListener('click',()=>{
     wrapper.classList.remove('active-popup');
 });
+const form = document.querySelector('form');
+const emailField = form.querySelector('input[type="email"]');
+const passwordField = form.querySelector('input[type="password"]');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  if (!isValidEmail(emailField.value)) {
+    alert('Please enter a valid email address.');
+    return;
+  }
+
+  if (!isValidPassword(passwordField.value)) {
+    alert('Please enter a valid password.');
+    return;
+  }
+
+  form.submit();
+});
+
+function isValidEmail(email) {
+  const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  return emailRegex.test(email);
+}
+
+function isValidPassword(password) {
+  return password.length >= 8;
+}
