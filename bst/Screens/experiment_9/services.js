6d5aeb76e7b0e6d7e7be6f2813e218fb3a03ef79
Kaisar bhat <kaisarahmadbhat1108@gmail.com>
2023-05-24 21:26:50 +0530

    experiment_9 added

---
+++ bst/Screens/experiment_9/services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Services = () => {
+  return (
+    <div>
+      <h2>Services</h2>
+      <p>Our services...</p>
+    </div>
+  );
+};
+
+export default Services;
